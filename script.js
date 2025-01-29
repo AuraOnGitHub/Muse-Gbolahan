e6fd535975ad66ebb965ef792321f12dd236fd1a
AuraOnGitHub <omotolabolarinwa15@gmail.com>
2025-01-29 14:48:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.home-icon').addEventListener('click', () => {
+  document.querySelector('.welcome-section').scrollIntoView({ behavior: 'smooth' });});
+
+document.querySelector('.about-icon').addEventListener('click', () => {
+  document.querySelector('.about-section').scrollIntoView({ behavior: 'smooth' });}  );
+
+document.querySelector('.project-icon').addEventListener('click', () => {
+  document.querySelector('.project-section').scrollIntoView({ behavior: 'smooth' });}  );
+
+document.querySelector('.contact-icon').addEventListener('click', () => {
+  document.querySelector('.contact-section').scrollIntoView({ behavior: 'smooth' });} );
